0b055b910ecd0179f091c6d44386e17b8b8e0d20
TinGoll <roma.oliynyk83@gmail.com>
2025-08-29 22:34:14 +0300

    config eslint

--- client/eslint.config.ts
+++ client/eslint.config.ts
@@ -1,78 +1,85 @@
 // eslint.config.ts
-import js from "@eslint/js";
-import prettier from "eslint-config-prettier";
-import importPlugin from "eslint-plugin-import";
-import prettierPlugin from "eslint-plugin-prettier";
-import reactHooks from "eslint-plugin-react-hooks";
-import reactRefresh from "eslint-plugin-react-refresh";
-import reactX from "eslint-plugin-react-x";
-import unusedImports from "eslint-plugin-unused-imports";
-import vitest from "eslint-plugin-vitest";
-import globals from "globals";
-import tseslint from "typescript-eslint";
+import js from '@eslint/js';
+import prettier from 'eslint-config-prettier';
+import importPlugin from 'eslint-plugin-import';
+import prettierPlugin from 'eslint-plugin-prettier';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import reactX from 'eslint-plugin-react-x';
+import unusedImports from 'eslint-plugin-unused-imports';
+import vitest from 'eslint-plugin-vitest';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
 
 export default tseslint.config([
   js.configs.recommended,
   tseslint.configs.recommended,
-  reactHooks.configs["recommended-latest"],
+  reactHooks.configs['recommended-latest'],
   reactRefresh.configs.vite,
 
   {
-    name: "custom:base",
-    files: ["**/*.{ts,tsx}"],
+    name: 'custom:base',
+    files: ['**/*.{ts,tsx}'],
     languageOptions: {
-      ecmaVersion: "latest",
-      sourceType: "module",
+      ecmaVersion: 'latest',
+      sourceType: 'module',
       globals: globals.browser,
     },
-    extends: [prettier, reactX.configs["recommended-typescript"]],
+    extends: [prettier, reactX.configs['recommended-typescript']],
     plugins: {
       import: importPlugin,
-      "unused-imports": unusedImports,
+      'unused-imports': unusedImports,
       prettier: prettierPlugin,
     },
     settings: {
       react: {
-        version: "detect",
+        version: 'detect',
       },
     },
     rules: {
-      "no-console": "warn",
-      "no-debugger": "warn",
+      'no-console': 'warn',
+      'no-debugger': 'warn',
 
       // imports
-      "unused-imports/no-unused-imports": "warn",
-      "import/order": [
-        "warn",
+      'unused-imports/no-unused-imports': 'warn',
+      'import/order': [
+        'warn',
         {
           groups: [
-            "builtin",
-            "external",
-            "internal",
-            "parent",
-            "sibling",
-            "index",
+            'builtin',
+            'external',
+            'internal',
+            'parent',
+            'sibling',
+            'index',
           ],
           pathGroups: [
             {
-              pattern: "@{app,pages,widgets,features,entities,shared}/**",
-              group: "internal",
-              position: "after",
+              pattern: '@{app,pages,widgets,features,entities,shared}/**',
+              group: 'internal',
+              position: 'after',
             },
           ],
-          pathGroupsExcludedImportTypes: ["builtin"],
-          alphabetize: { order: "asc", caseInsensitive: true },
-          "newlines-between": "always",
+          pathGroupsExcludedImportTypes: ['builtin'],
+          alphabetize: { order: 'asc', caseInsensitive: true },
+          'newlines-between': 'always',
         },
       ],
 
-      "prettier/prettier": "warn",
+      'prettier/prettier': [
+        'warn',
+        {
+          endOfLine: 'crlf',
+          semi: true,
+          singleQuote: true,
+        },
+      ],
     },
   },
 
   {
-    name: "custom:tests",
-    files: ["**/*.test.{ts,tsx}", "**/__tests__/**/*.{ts,tsx}"],
+    name: 'custom:tests',
+    files: ['**/*.test.{ts,tsx}', '**/__tests__/**/*.{ts,tsx}'],
     plugins: { vitest },
     extends: [vitest.configs.recommended],
     languageOptions: {
@@ -84,6 +91,6 @@ export default tseslint.config([
   },
 
   {
-    ignores: ["dist", "build", "coverage", "node_modules", ".eslintcache"],
+    ignores: ['dist', 'build', 'coverage', 'node_modules', '.eslintcache'],
   },
 ]);
