6ecfa84f3fd8b4b55dbc99d77ac901fbb4f1b290
TinGoll <roma.oliynyk83@gmail.com>
2025-08-31 12:00:13 +0300

    module-employees: add entity

--- server/src/modules/employees/dto/create-employee.dto.ts
+++ server/src/modules/employees/dto/create-employee.dto.ts
@@ -1,1 +1,17 @@
-export class CreateEmployeeDto {}
+import { EmployeeStatus } from '../entities/employee.entity';
+
+export class CreateEmployeeDto {
+  name: string;
+  sectorId?: number;
+  department?: string;
+  status: EmployeeStatus;
+  location?: string;
+  firstName?: string;
+  lastName?: string;
+  middleName?: string;
+  bankCard?: string;
+  cardHolder?: string;
+  phone?: string;
+  permissionGroupId?: number;
+  mgmtPass?: string;
+}
